0ac55b81dcd7ead6fe68cf92a8d1dec683323359
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-02-06 15:52:05 +0100

    refactor: Remove spaces from configuration options and values

--- app/classes/Configuration.php
+++ app/classes/Configuration.php
@@ -5,6 +5,9 @@ namespace App\Classes;
 class Configuration {
     private array $configuration;
 
+    /**
+     * Read a configuration file
+     */
     public function __construct(private string $config_file = '../.env')
     {
         // Get data from the .env
@@ -13,11 +16,17 @@ class Configuration {
         foreach($config_data as $config) {
             $current_option = explode('=', $config);
 
-            $this->configuration[$current_option[0]] = trim($current_option[1]);
+            $this->configuration[trim($current_option[0])] = trim($current_option[1]);
         }
     }
 
-    public function getParameter($param): string
+    /**
+     * Get configuration parameter
+     * 
+     * @param string $param
+     * @return string
+     */
+    public function getParameter(string $param): string
     {
         return $this->configuration[$param];
     }
